15e998efa017cf1d86a7cda5cefa7016cd20d45b
OMKAR9395 <omkarmohite85@gmail.com>
2026-02-11 20:36:11 +0530

    Batch 02

---
+++ Session 01/Program.js
@@ -0,0 +1,8 @@
+console.log("Hello From My Terminal");
+
+let num1 = 10
+let num2 = 20
+
+let sum = num1 + num2
+
+console.log(sum)
---
+++ Session 02/001.js
@@ -0,0 +1,10 @@
+// console.log(a);
+
+let a = 10;
+
+ console.log(a);
+ console.log(b);
+
+ var b = 20;
+
+ console.log(b);
